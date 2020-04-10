9fda5752cbab2317b84220062cd71c53f750edad
dongdawn <dongdong1120@foxmail.com>
2020-04-11 03:50:57 +0800

    Create hydrophobic.tcl

---
+++ vmd_script/hydrophobic.tcl
@@ -0,0 +1,2 @@
+#MIanal HYDROPHOBIC $molid 40.0 100.0 0.05 4.0 "resname ALA PHE GLY ILE LEU MET PRO VAL TYR and name \"C.*\" and not name C" "resname ALA PHE GLY ILE LEU MET PRO VAL TYR and name \"C.*\" and not name C"
+MIresAnal HYDROPHOBIC $molid 0.0 4.0 "resname ALA PHE GLY ILE LEU MET PRO VAL TYR and name \"C.*\" and not name C" "resname ALA PHE GLY ILE LEU MET PRO VAL TYR and name \"C.*\" and not name C"
